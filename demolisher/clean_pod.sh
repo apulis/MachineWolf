d277f12d3ef88b178a73cdacf75da6f970f7eef0
haiyuan <haiyuan.bian@apulis.com>
2021-01-31 23:36:59 +0800

    add cleaner script to kill un-healthy

--- demolisher/clean_pod.sh
+++ demolisher/clean_pod.sh
@@ -15,14 +15,3 @@ do
     done
   done
 done
-
-
-# 添加docker用户组，一般已存在，不需要执行
-sudo groupadd docker     
-# 将登陆用户加入到docker用户组中
-sudo gpasswd -a $USER docker
-# 更新用户组
-newgrp docker   
-# 测试docker命令是否可以使用sudo正常使用
-docker version  
-
